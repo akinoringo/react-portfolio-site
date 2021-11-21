bc3bb4532593d957de03d095c6eae73d61db87ec
akinoringo <wbraver@outlook.jp>
2021-11-21 10:31:17 +0900

    feat: finish header base coding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,24 @@
-export const Header = () => {
-  const componentName = () => 'Header';
+import CoverImage from '../images/cover.jpeg'
+import ProfileImage from '../images/profile.png'
 
+
+export const Header = () => {
   return (
-    <div className="test-wrapper">
-      このコンポーネントの名前は{componentName()}です。
-    </div>
+    <header className="main-cover" style={{ backgroundImage: `url(${CoverImage})` }}>
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="display-table">
+          <div className="display-table-contents">
+            <div className="profile-thumb" style={{ backgroundImage: `url(${ProfileImage})` }}></div>
+            <h1 className="title-text">Matsumoto Akinori</h1>
+            <h3 className="title-text">WEB Engineer</h3>
+            <ul className="social-links">
+              <li className="icon-link"></li>
+              <li className="icon-link"></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
